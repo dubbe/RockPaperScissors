2b8ae1fea7e65dbca507384f2149a70a3e0aba28
Thomas Dahlberg <thomas@dhlbrg.se>
2019-06-08 09:33:42 +0200

    Added dockerfile

--- src/RockPaperScissors/Startup.cs
+++ src/RockPaperScissors/Startup.cs
@@ -45,7 +45,7 @@ namespace RockPaperScissors
                 app.UseHsts();
             }
             
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
